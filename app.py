b36f727e280f6501cb7b9523dc74e9a6a69afbf2
carlosEduardo-hub <carloseduardo010616@gmail.com>
2023-07-20 16:57:29 -0300

    commit templates

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template , url_for
+
+
+app = Flask(__name__, static_folder='static')
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+    
+@app.route('/pictures_add')
+def pictures_add():
+    return render_template("pictures_add.html")  
+
+@app.route('/perfil')
+def perfil():
+    return render_template("perfil.html")
+
+@app.route('/gallery')
+def gallery():
+    return render_template("gallery.html")
+
+@app.route('/logout')
+def logout():
+    return render_template("login.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
