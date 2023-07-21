68a398b9bbc34629349f2c23d5cfbe89418362e2
brunoestrella <brunogamer2002@gmail.com>
2023-07-21 18:01:45 -0300

    integrando com as telas

--- projeto/routes.py
+++ projeto/routes.py
@@ -1,53 +1,76 @@
 from projeto import app
 from projeto import db
 from projeto.validators import validate_email, validate_senha, validate_usuario
-from projeto.models import User
+from projeto.models import User, Uploads
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 
-@app.route('/', methods=['POST', 'GET'])
-def page_home():
-    return 'HelloWorld'
 
-
-@app.route('/register', methods=['POST', 'GET'])
-def page_register():
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
     if request.method == 'POST':
 
-        nome = request.form['nome']
-        usuario = request.form['usuario']
-        email = request.form['email']
-        senha = request.form['senha']
+        nome = request.form.get('nome')
+        usuario = request.form.get('usuario')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        confirma_senha = request.form.get('confirmar_senha')
 
-        if validate_usuario(usuario) and validate_email(email) and validate_senha(senha):
-            user = User(nome, usuario, email, senha)
-            db.session.add(user)
-            db.session.commit()
-            return redirect(url_for('page_login'))
-        
+        if validate_email(email) and validate_usuario(usuario) and validate_senha(senha):
+            if senha == confirma_senha:
+                user = User(nome=nome,
+                            usuario=usuario,
+                            email=email,
+                            senhacrip=senha)
+                
+                db.session.add(user)
+                db.session.commit()
+                return redirect(url_for('login'))
         else:
             flash('Erro ao cadastrar', category='danger')
 
-    return render_template('register.html')
+    return render_template('signup.html')
 
 
-@app.route('/login', methods=['POST', 'GET'])
-def page_login():
+@app.route('/', methods=['POST', 'GET'])
+def login():
     if request.method == 'POST':
-        email = request.form['email']
-        senha = request.form['senha']
+        email = request.form.get('email')
+        senha = request.form.get('senha')
 
         user_logged = User.query.filter_by(email=email).first()
         
         if user_logged and user_logged.converte_senha(senha_texto_claro=senha):
-            return redirect(url_for('page_home'))
+            login_user(user_logged)
+            return redirect(url_for('dashboard'))
         else:
             flash('Erro ao logar', category='danger')
 
     return render_template('login.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+    
+@app.route('/pictures_add')
+@login_required
+def pictures_add():
+    return render_template("pictures_add.html")  
+
+@app.route('/perfil')
+@login_required
+def perfil():
+    users = User.query.all()
+    #perfil = User.query.filter_by(id=user_id).first
+    return render_template("perfil.html", users=users)
+
+@app.route('/gallery')
+@login_required
+def gallery():
+    return render_template("gallery.html")
 
 @app.route('/logout')
-def page_logout():
+def logout():
     logout_user()
     return redirect(url_for('login'))
+    
--- projeto/validators.py
+++ projeto/validators.py
@@ -1,22 +1,22 @@
 from projeto.models import User
 
 
-def validate_usuario(self, check_user):
-    user = User.query.filter_by(usuario=check_user.data).first()
+def validate_usuario(check_user):
+    user = User.query.filter_by(usuario=check_user).first()
     if user:
         return False
     else:
         return True
 
-def validate_email(self, check_email):
-    email = User.query.filter_by(email=check_email.data).first()
+def validate_email(check_email):
+    email = User.query.filter_by(email=check_email).first()
     if email:
         return False
     else:
         return True
 
-def validate_senha(self, check_senha):
-    senha = User.query.filter_by(senha=check_senha.data).first()
+def validate_senha(check_senha):
+    senha = User.query.filter_by(senha=check_senha).first()
     if senha:
         return False
     else:
