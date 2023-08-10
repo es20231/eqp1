bc4eac6adda22ad8ca82cd629e22dad0feba287e
brunoestrella <brunogamer2002@gmail.com>
2023-08-10 17:01:33 -0300

    tabelas like,post,com

--- projeto/models.py
+++ projeto/models.py
@@ -15,7 +15,18 @@ class User(db.Model, UserMixin):
     usuario = db.Column(db.String(length=50), nullable=False, unique=True)
     email = db.Column(db.String(length=100), nullable=False, unique=True)
     senha = db.Column(db.String(length=15), nullable=False, unique=True)
+    bio = db.Column(db.String(length=1024))
+    perfil_photo = db.Column(db.LargeBinary)
+    posts = db.relationship('Posts', backref='dono_user', lazy=True)
     uploads = db.relationship('Uploads', backref='dono_user', lazy=True)
+
+    def add_perfil_photo(self, data):
+        self.perfil_photo = data
+        db.session.commit()
+
+    def add_bio(self, desc):
+        self.bio = desc
+        db.session.commit()
 
     @property
     def senhacrip(self):
@@ -29,6 +40,7 @@ class User(db.Model, UserMixin):
     # Converte a senha criptografada para o normal e a compara com uma outra senha. Deve-se chamar esse método ao realizar o login do usuário.
     def converte_senha(self, senha_texto_claro):
         return bcrypt.check_password_hash(self.senha, senha_texto_claro)
+    
 
 
 class Uploads(db.Model):
@@ -36,9 +48,34 @@ class Uploads(db.Model):
     data = db.Column(db.LargeBinary, nullable=False)
     usuario = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, data):
-        self.data = data
-    
-    def insert_logged_user_id(self, user_logged):
+    def upload_foto(self, user_logged):
         self.usuario = user_logged.id
         db.session.commit()
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.LargeBinary, nullable=False)
+    desc =  db.Column(db.String(length=1024))
+    quant_curtidas = db.Column(db.Integer, default=0)
+    usuario = db.Column(db.Integer, db.ForeignKey('user.id'))
+    curtidas = db.relationship('Likes', backref='like_user', lazy=True)
+    comentarios = db.relationship('Comments', backref='com_user', lazy=True)
+
+    def curtir(self):
+        self.quant_curtidas += 1
+        db.session.commit()
+
+
+class Likes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(length=50), nullable=False)
+    post = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(length=50), nullable=False)
+    comentario = db.Column(db.String(length=1024), nullable=False)
+    post = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    
