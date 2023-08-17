459ee51632b44f5dfd718874d7c2e06c0593218c
José Gayoso Neto <josegayosoneto@outlook.com.br>
2023-08-17 01:03:44 -0300

    Alterando MAX_CONTENT_LENGTH config

--- projeto/__init__.py
+++ projeto/__init__.py
@@ -6,16 +6,14 @@ from flask_mail import Mail
 from itsdangerous import URLSafeTimedSerializer
 import os
 from dotenv import load_dotenv
-from flask_socketio import SocketIO
 
 
 load_dotenv()
 db = SQLAlchemy()
 app = Flask(__name__)
-socketio = SocketIO(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
+app.config["MAX_CONTENT_LENGTH"] = 20 * 1024 * 1024
 db.init_app(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager()
