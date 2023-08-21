0bc1786a5db9bf0719cdf09f00c9422f58a4ab5e
brunoestrella <brunogamer2002@gmail.com>
2023-08-21 09:52:32 -0300

    novos filtros

--- projeto/filters.py
+++ projeto/filters.py
@@ -1,4 +1,8 @@
-from PIL import Image
+from PIL import Image, ImageFilter
+from PIL.ImageFilter import (
+   BLUR, CONTOUR, DETAIL, EDGE_ENHANCE, EDGE_ENHANCE_MORE,
+   EMBOSS, FIND_EDGES, SMOOTH, SMOOTH_MORE, SHARPEN
+)
 import io
 
 def black_and_white(og_img):
@@ -12,5 +16,45 @@ def black_and_white(og_img):
 
     return img_bytes
 
+def blur_filter(og_img):
+    image = Image.open(io.BytesIO(og_img))
+    img_blur = image.filter(BLUR)
 
+    img_io = io.BytesIO()
+    img_blur.save(img_io, format='JPEG')
+    img_bytes = img_io.getvalue()
+
+    return img_bytes
+
+
+def sharpen_filter(og_img):
+    image = Image.open(io.BytesIO(og_img))
+    img_sharpen = image.filter(SHARPEN)
+
+    img_io = io.BytesIO()
+    img_sharpen.save(img_io, format='JPEG')
+    img_bytes = img_io.getvalue()
+
+    return img_bytes
+
+
+def smooth_filter(og_img):
+    image = Image.open(io.BytesIO(og_img))
+    img_smooth = image.filter(SMOOTH)
+
+    img_io = io.BytesIO()
+    img_smooth.save(img_io, format='JPEG')
+    img_bytes = img_io.getvalue()
+
+    return img_bytes
+
+
+
+
+
+    
+
+
+
+    
 
